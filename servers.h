8cf8304deabb355323df3c08ba308507d5c63c28
Humbedooh <github@cord.dk>
2011-01-06 17:49:31 +0100

    Added inits for pop3 and imap servers Added servers.h (server init prototypes)
    Signed-off-by: Humbedooh <github@cord.dk>

---
+++ servers.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   servers.h
+ * Author: Administrator
+ *
+ * Created on January 6, 2011, 3:39 PM
+ */
+
+#ifndef SERVERS_H
+#define	SERVERS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void* rumble_smtp_init(void* m);
+void* rumble_pop3_init(void* m);
+void* rumble_imap_init(void* m);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SERVERS_H */
+
