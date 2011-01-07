e9c89f38884985932d872f5c9087adde6e7343c4
Humbedooh <github@cord.dk>
2011-01-08 00:07:42 +0100

    forgot to add hooks.c :\
    Signed-off-by: Humbedooh <github@cord.dk>

---
+++ hooks.c
@@ -0,0 +1,75 @@
+#include "rumble.h"
+#include "servers.h"
+
+void rumble_hook_function(void* handle, uint32_t flags, ssize_t (*func)(sessionHandle*) ) {
+    hookHandle* hook = malloc(sizeof(hookHandle));
+    hook->func = func;
+    hook->flags = flags;
+    switch ( flags && RUMBLE_HOOK_STATE_MASK ) {
+        case RUMBLE_HOOK_ACCEPT:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: cvector_add(((masterHandle*) handle)->smtp.init_hooks, hook);break;
+                case RUMBLE_HOOK_POP3: cvector_add(((masterHandle*) handle)->pop3.init_hooks, hook);break;
+                case RUMBLE_HOOK_IMAP: cvector_add(((masterHandle*) handle)->imap.init_hooks, hook);break;
+                default: break;
+                } break;
+        case RUMBLE_HOOK_COMMAND:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: cvector_add(((masterHandle*) handle)->smtp.cue_hooks, hook);break;
+                case RUMBLE_HOOK_POP3: cvector_add(((masterHandle*) handle)->pop3.cue_hooks, hook);break;
+                case RUMBLE_HOOK_IMAP: cvector_add(((masterHandle*) handle)->imap.cue_hooks, hook);break;
+                default: break;
+                } break;
+        case RUMBLE_HOOK_EXIT:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: cvector_add(((masterHandle*) handle)->smtp.exit_hooks, hook);break;
+                case RUMBLE_HOOK_POP3: cvector_add(((masterHandle*) handle)->pop3.exit_hooks, hook);break;
+                case RUMBLE_HOOK_IMAP: cvector_add(((masterHandle*) handle)->imap.exit_hooks, hook);break;
+                default: break;
+                } break;
+        default: break;
+    }
+}
+
+
+ssize_t rumble_server_execute_hooks(sessionHandle* session, cvector* hooks, unsigned short flags) {
+    int g = 0;
+    ssize_t rc = EXIT_SUCCESS;
+    hookHandle* el;
+    for (el = (hookHandle*) cvector_first(hooks); el != NULL; el = (hookHandle*) cvector_next(hooks)) {
+        if ( el->flags == flags ) {
+            g++;
+            ssize_t (*hookFunc)(sessionHandle*) = el->func;
+            rc = (*hookFunc)(session);
+        }
+    }
+    return rc;
+}
+
+ssize_t rumble_server_schedule_hooks(masterHandle* handle, sessionHandle* session, unsigned short flags) {
+    switch ( flags && RUMBLE_HOOK_STATE_MASK ) {
+        case RUMBLE_HOOK_ACCEPT:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: return rumble_server_execute_hooks(session, handle->smtp.init_hooks, flags);
+                case RUMBLE_HOOK_POP3: return rumble_server_execute_hooks(session, handle->pop3.init_hooks, flags);
+                case RUMBLE_HOOK_IMAP: return rumble_server_execute_hooks(session, handle->imap.init_hooks, flags);
+                default: break;
+                } break;
+        case RUMBLE_HOOK_COMMAND:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: return rumble_server_execute_hooks(session, handle->smtp.cue_hooks, flags);
+                case RUMBLE_HOOK_POP3: return rumble_server_execute_hooks(session, handle->pop3.cue_hooks, flags);
+                case RUMBLE_HOOK_IMAP: return rumble_server_execute_hooks(session, handle->imap.cue_hooks, flags);
+                default: break;
+            } break;
+        case RUMBLE_HOOK_EXIT:
+            switch ( flags & RUMBLE_HOOK_SVC_MASK ) {
+                case RUMBLE_HOOK_SMTP: return rumble_server_execute_hooks(session, handle->smtp.exit_hooks, flags);
+                case RUMBLE_HOOK_POP3: return rumble_server_execute_hooks(session, handle->pop3.exit_hooks, flags);
+                case RUMBLE_HOOK_IMAP: return rumble_server_execute_hooks(session, handle->imap.exit_hooks, flags);
+                default: break;
+            } break;
+        default: break;
+    }
+    return EXIT_SUCCESS;
+}
