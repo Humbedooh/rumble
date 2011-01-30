088ca2bb13bc5e154acd73d6e8d70e99d4dc9fa9
Humbedooh <github@cord.dk>
2011-01-30 21:54:50 +0100

    added rumble_lua.h for later use.

---
+++ src/rumble_lua.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   rumble_lua.h
+ * Author: Administrator
+ *
+ * Created on January 30, 2011, 5:15 AM
+ */
+
+#ifndef RUMBLE_LUA_H
+#define	RUMBLE_LUA_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <lua.h>
+#include <lualib.h>
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* RUMBLE_LUA_H */
+
