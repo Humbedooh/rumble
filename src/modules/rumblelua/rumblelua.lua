d8b66e115eaba7d3452302679b47326890510120
Humbedooh <github@cord.dk>
2011-03-20 21:01:41 +0100

    Added Mailman.updateDomain(domain, newname, newpath, newtype) for updating existing domains Updated RumbleLua quite a bit.

--- src/modules/rumblelua/rumblelua.lua
+++ src/modules/rumblelua/rumblelua.lua
@@ -8,23 +8,29 @@ function getFile(filename)
 	local ret = "";
 	for k, entry in pairs(fileCache) do
 		if ( entry.filename == filename ) then
-			found = true;
 			local fstat = file.stat(filename);
-			if ( entry.modified ~= fstat.modified ) then
-				local f = io.open(filename, "rb");
-				if ( f ) then 
-					entry.contents = f:read("*a");
-					f:close();
+			if (fstat) then
+				found = true;
+				if ( entry.modified ~= fstat.modified ) then
+					local f = io.open(filename, "rb");
+					if ( f ) then 
+						entry.contents = f:read("*a");
+						f:close();
+					end
 				end
+				ret = entry.contents;
+			else
+				ret = "";
 			end
-			ret = entry.contents;
 			break;
 		end
 	end
 	if (not found) then
 		local entry = { filename = filename };
 		local fstat = file.stat(filename);
-		entry.modified = fstat.modified;
+		if (fstat) then
+			entry.modified = fstat.modified;
+		end
 		entry.contents = "";
 		local f = io.open(filename, "rb");
 		if (f) then
@@ -38,7 +44,7 @@ function getFile(filename)
     return ret;
 end
 
-function comma(number)
+function math.comma(number)
  local left,num,right = string.match(number,'^([^%d]*%d)(%d+)(.-)$');
 local prettyNum = (left and left..(num:reverse():gsub('(%d%d%d)','%1,'):reverse()) or number);
 return prettyNum;
@@ -123,13 +129,15 @@ function acceptHTTP(session)
     local d = debug.getinfo(1);
     session.path = session.info.path .. "/" .. d.short_src:match("^(.-)%w+%.lua$");
     local auth = {};
-    
+    local firstVisit = true;
+	
 	--session:lock();
     session.output = getFile(session.path .. "/template.html");
     local config = getFile(session.path .. "/auth.cfg");
 	--session:unlock();
 	
     for user,pass,rights in config:gmatch("([^:\r\n]+):([^:\r\n]+):([^:\r\n]+)") do
+		firstVisit = false;
         local domains = {};
         local admin = false;
         if ( rights == "*" ) then admin = true;
@@ -156,18 +164,27 @@ function acceptHTTP(session)
             end
         end
     end
-    if (not session.credentials) then
-        session:send("HTTP/1.1 401 Authorization Required\r\nWWW-Authenticate: Basic realm=\"RumbleLua\"\r\nContent-Type: text/html\r\n\r\nAuthorization required!\n");
-        return;
-    end
-    session:send("HTTP/1.1 200 OK\r\n");
-	session:send("Connection: close\r\n");
+	if (firstVisit and http.URL == "") then
+		session:send("HTTP/1.1 302 Moved\r\n");
+		session:send("Location: /welcome\r\n\r\n");
+		return;
+	elseif (not firstVisit) then
+		if (not session.credentials) then
+			session:send("HTTP/1.1 401 Authorization Required\r\nWWW-Authenticate: Basic realm=\"RumbleLua\"\r\nContent-Type: text/html\r\n\r\nAuthorization required!\n");
+			return;
+		end
+	end
+    
+	
     --[[ Then, check if a specific file was requested rather than an action ]]--
-    if ( http.URL:len() > 0 and not http.URL:match("auth%.cfg")) then
+    if ( not http.URL:match("auth%.cfg")) then
 		--session:lock();
+		if (http.URL:len() == 0) then http.URL = "index"; end
 		local exists = file.exists(session.path .. http.URL);
 		--session:unlock();
         if (exists) then
+			session:send("HTTP/1.1 200 OK\r\n");
+			session:send("Connection: close\r\n");
             output = getFile(session.path .. http.URL);
 			
 			if ( http.URL:match("%.png")) then session:send("Content-Type: image/png\r\n");
@@ -179,30 +196,50 @@ function acceptHTTP(session)
 			if (output) then session:send(output:len(), output); end
 			output = nil;
 			return;
+		else 
+			local section,subSection = http.URL:lower():match("^([^:]+):?(.-)$");
+			session.section = subSection;
+			local scriptFile = session.path .. "/scripts/" .. section .. ".phtml";
+			if ( http.URL == "" ) then scriptFile = session.path .. "/scripts/index.phtml"; end
+			if (file.exists(scriptFile) and not (firstVisit and http.URL ~= "welcome")) then
+				script = getFile(scriptFile);
+			else
+				session:send("HTTP/1.1 302 Moved\r\n");
+				session:send("Location: /\r\n\r\n");
+				return;
+			end
+			script = script:gsub("<%?=(.-)%?>", function(x) _G.session = session; local ret, val = pcall(loadstring("return ("..x..")")); return val or "meh"; end);
+			script = script:gsub("<%?(.-)%?>", 
+				function(x) 
+					_G.session = session;
+					local output = "";
+					local _printf = printf;
+					local xit = _G.exit;
+					_G.exit = function() session.killed = true; return; end
+					_G.printf = function(...) output = output .. string.format(...); end;
+					loadstring(x)(); 
+					_G.printf = _printf;
+					_G.exit = xit;
+					return output;
+				end);
+			session.contents = script:gsub("%[%%contents%%%]", script);
 		end
+		
      end
     
     --[[ Otherwise, start the action! ]]--
-    session:send("Content-Type: text/html\r\n");
-    session:send("\r\n");
-    session.output = session.output:gsub("%[%%header%%%]", ("RumbleLua on %s"):format(servername));
-    session.output = session.output:gsub("%[%%title%%%]", "Main page");
-	session.output = session.output:gsub("%[%%version%%%]", session.info.version);
-    session.output = session.output:gsub("%[%%footer%%%]", ("Powered by Rumble Mail Server v/%s - %s"):format(session.info.version, os.date()));
-    local section,subSection = http.URL:lower():match("^([^:]+):?(.-)$");
-    
-    session.contents = "";
-    if (section == "domains") then
-        if (subSection ~= "") then accountsPage(session,subSection);
-        else domainPage(session);
-        end
-       
-    else
-        mainPage(session);
-    end
-    session.output = session.output:gsub("%[%%contents%%%]", session.contents);
-    session:send(session.output);
-	
+	if (not session.killed) then
+		session:send("HTTP/1.1 200 OK\r\n");
+		session:send("Connection: close\r\n");
+		session:send("Content-Type: text/html\r\n");
+		session:send("\r\n");
+		session.output = session.output:gsub("%[%%header%%%]", ("RumbleLua on %s"):format(servername));
+		session.output = session.output:gsub("%[%%title%%%]", "Main page");
+		session.output = session.output:gsub("%[%%version%%%]", session.info.version);
+		session.output = session.output:gsub("%[%%footer%%%]", ("Powered by Rumble Mail Server v/%s - %s"):format(session.info.version, os.date()));
+		session.output = session.output:gsub("%[%%contents%%%]", session.contents);
+		session:send(session.output);
+	end
 	session.credentials = nil;
 	auth = nil;
 	session = {};
@@ -238,249 +275,6 @@ function parseLua(data)
     print = _print;
     printf = _printf;
     _LUA_TMP = prev;
-end
-
-
-function mainPage(session)
-    append("<h2>Server information</h2>");
-    append("<table class=\"elements\" border='0' cellspacing='1' cellpadding='5'>");
-    append("<tr><td>Version:</td><td class='plain_text'>%s</td></tr>", session.info.version);
-	append("<tr><td>System:</td><td class='plain_text'>%s (%s)</td></tr>", session.info.os, (session.info.arch == 32 and "x86") or "x64");
-    append("<tr><td>Location:</td><td class='plain_text'>%s</td></tr>", session.info.path);
-    local hours = math.floor(session.info.uptime/3600);
-    local minutes = math.floor(math.fmod(session.info.uptime, 3600)/60);
-    local seconds = math.fmod(session.info.uptime, 60);
-    append("<tr><td>Uptime:</td><td class='plain_text'>%02u:%02u:%02u</td></tr>", hours,minutes,seconds);
-    append("</table>");
-    
-    append("<br/><br/><h2>Services</h2>");
-    for k,v in pairs({"smtp", "imap", "pop3", "core"}) do
-        local svc = Rumble.serviceInfo(v);
-        if (svc) then
-            append("<table class=\"elements\" border='0' cellspacing='1' cellpadding='5'>");
-            append("<tr><th colspan='2'>%s</th></tr>", v:upper());
-            if (svc.enabled) then 
-                append("<tr><td>Status:</td><td class='plain_text'><font color='darkgreen'>Enabled</font></td></tr>");
-                append("<tr><td>Threads:</td><td class='plain_text'>%u total, %u working, %u idling.</td></tr>", svc.workers, svc.busy, svc.idle);
-                append("<tr><td>Traffic:</td><td class='plain_text'>%s sessions, %s bytes received, %s bytes sent</td></tr>", comma(svc.sessions), comma(svc.received), comma(svc.sent));
-				if (svc.capabilities:len() > 0) then
-					append("<tr><td>Capabilities:</td><td class='plain_text'>%s</td></tr>", svc.capabilities);
-				end	
-            else 
-                append("<tr><td>Status:</td><td class='plain_text'><font color='darkred'>Disabled</font></td></tr>");
-            end
-            append("</table>");
-        end
-    end
-    
-    append("<br/><br/><h2>Modules</h2>");
-    for k,mod in pairs(Rumble.listModules()) do
-        local file = mod.file;
-        if not (file:match("^/") or file:match(":")) then
-            file = session.info.path .. "/" ..file;
-        end
-        append("<table class=\"elements\" border='0' cellspacing='1' cellpadding='5'>");
-        append("<tr><th colspan='2'>%s</th></tr>", mod.title);
-        append("<tr><td>Description:</td><td class='plain_text'>%s</td></tr>", mod.description);
-        if (mod.author ~= "Unknown") then append("<tr><td>Author:</td><td class='plain_text'>%s</td></tr>", mod.author); end
-        append("<tr><td>File:</td><td class='plain_text'>%s</td></tr>", file);
-        append("</table>");
-    end
-end
-
-
-function domainPage(session, domain)
-append("<h2>Domains</h2>", domain);
-    
-    
-    
-    
-    if (session.credentials.admin) then
-    append("<table class=\"elements\" border='0' cellpadding='5' cellspacing='1'>");
-    append("<tr><th>Create a new domain</th></tr>");
-    
-        if (http.form.domain) then
-            if (http.form.delete) then
-                if (Mailman.deleteDomain(http.form.domain)) then
-                    append("<tr><td><b><font color='red'>Deleted domain %s.</font></b></td></tr>", http.form.domain);
-                else
-                    append("<tr><td><b><font color='red'>Could not delete domain %s!</font></b></td></tr>", http.form.domain);
-                end
-            elseif (http.form.create) then
-                Mailman.createDomain(http.form.domain, http.form.path or "");
-                append("<tr><td><b><font color='darkgreen'>Domain %s has been created.</font></b></td></tr>", http.form.domain);
-            end
-        end
-        
-        append("<tr><td>");
-        append([[
-        <form action="/domains" method="post" id='create'>
-        
-        <div class='form_el' style='display: block;' id='domainname'>
-            <div class='form_key'>
-                Domain name:
-            </div>
-            <div class='form_value'>
-                <input type="text" name="domain"/>
-            </div>
-        </div>
-        
-        <div class='form_el' style='display: block;' id='domainname'>
-            <div class='form_key'>
-                Optional alt. storage path:
-            </div>
-            <div class='form_value'>
-                <input type="text" name="path"/>
-            </div>
-        </div>
-        
-        <input type="hidden" name="create" value="true"/>
-        <div class='form_el' id='domainsave' >
-            <input type="submit" value="Save domain"/>
-        </div>
-        </form>
-        ]]);
-        append("</td></tr>");
-        
-        
-        append("</table>");
-    end
-    
-    append("<table class=\"elements\" border='0' cellpadding='5' cellspacing='1'>");
-    append("<tr><th>Domain</th><th>Type</th><th>Actions</th></tr>");
-    local t = Mailman.listDomains(); 
-    table.sort(t);
-    for k,v in pairs(t) do
-        if (session.credentials.admin or session.credentials.domains[v]) then
-            append("<tr><td><img src='/bullet.png' align='absmiddle'/><a href='/domains:%s'>%s</a></td><td>domain</td><td>[Edit] [<a href=\"/domains?domain=%s&delete=true\">Delete</a>]</td></tr>", v,v,v);
-        end
-    end
-    if (#t == 0) then
-        append("<tr><td colspan='3'><i>No domains are configured for this server yet.</i></td></tr>");
-    end
-    append("</table>");
-end
-
-
-function accountsPage(session, domain)
-    append("<h2>Accounts @ %s</h2>", domain);
-    
-    append("<table class=\"elements\" border='0' cellpadding='5' cellspacing='1'>");
-    append("<tr><th>Create a new account</th></tr>");
-    
-    
-    
-    if (http.form.user) then
-        if (http.form.delete) then
-            local acc = Mailman.readAccount(tonumber(http.form.user));
-            if ( acc ) then
-                Mailman.deleteAccount(tonumber(http.form.user));
-                append("<tr><td><b><font color='red'>Deleted account %s@%s.</font></b></td></tr>", acc.name, domain);
-            end
-            
-        elseif (http.form.create) then
-            if (Mailman.accountExists(domain, http.form.user)) then
-                append("<tr><td><b><font color='red'>Error: Account %s@%s already exists!</font></b></td></tr>", http.form.user, domain);
-            else
-                local acc = {};
-                acc.name = http.form.user;
-                acc.domain = domain;
-                acc.type = http.form.type or "mbox";
-                acc.password = SHA256(http.form.password or "");
-                acc.arguments = http.form.arguments or "";
-                Mailman.saveAccount(acc);
-                append("<tr><td><b><font color='darkgreen'>Account %s@%s has been created.</font></b></td></tr>", http.form.user, domain);
-            end
-        end
-    end
-    
-    append("<tr><td>");
-    append([[
-    
-    <form action="/domains:%s" method="post" id='create'>
-    <div>
-    <div class='form_el'>
-        <div class='form_key'>
-            Account type:
-        </div>
-        <div class='form_value'>
-            <select name="type" 
-            onChange="
-            document.getElementById('accname').style.display = 'inherit';
-            var type = this.options[this.selectedIndex].value;
-            document.getElementById('accsave').style.display = 'inherit';
-            if (type == '') {
-                document.getElementById('accarg').style.display = 'none';
-                document.getElementById('accname').style.display = 'none';
-                document.getElementById('accpass').style.display = 'none';
-                document.getElementById('accsave').style.display = 'none';
-                }
-            else if (type == 'mbox') { 
-                document.getElementById('accpass').style.display = 'inherit';
-                document.getElementById('accarg').style.display = 'none';
-            }
-            else {
-                document.getElementById('accpass').style.display = 'none';
-                document.getElementById('accarg').style.display = 'inherit';
-                }
-            "
-            >
-            <option value="">Select...</option>
-            <option value="mbox">Mailbox</option>
-            <option value="alias">Alias</option>
-            <option value="mod">Feed to module...</option>
-            <option value="feed">Feed to program...</option>
-            <option value="relay">Relay to other server</option>
-            </select>
-        </div>
-    </div>
-    <div class='form_el' style='display: none;' id='accname'>
-        <div class='form_key'>
-            Account name:
-        </div>
-        <div class='form_value'>
-            <input type="text" name="user"/> <i>@%s</i>
-        </div>
-    </div>
-    
-    <div class='form_el' style="display: none" id='accpass';>
-        <div class='form_key'>
-            Password:
-        </div>
-        <div class='form_value'>
-            <input type="password" name="password"/>
-        </div>
-    </div>
-    <div class='form_el' id='accarg' style='display: none;' >
-        <div class='form_key'>
-            Arguments: 
-        </div>
-        <div class='form_value'>
-            <input type="text" name="arguments"/>
-        </div>
-    </div>
-    <input type="hidden" name="create" value="true"/>
-    <div class='form_el' id='accsave' style='display: none;' >
-            <input type="submit" value="Save account"/>
-    </div>
-    </div>
-</form>
-    ]], domain,domain);
-    append("</td></tr>");
-    
-    
-    append("</table>");
-    
-    append("<table class=\"elements\" border='0' cellpadding='5' cellspacing='1'>");
-    append("<tr><th>Account</th><th>Type</th><th>Actions</th></tr>");
-    local t = Mailman.listAccounts(domain);
-    for k,v in pairs(t) do
-        append("<tr><td><a href='/accounts:%u'>%s@%s</a></td><td>%s</td><td>[Edit] [<a href=\"/domains:%s?user=%u&delete=true\">Delete</a>]</td></tr>", v.id,v.name,domain,v.type,domain,v.id);
-    end
-    if (#t == 0) then
-        append("<tr><td colspan='3'><i>%s does not have any mail accounts.</i></td></tr>", domain);
-    end
-    append("</table>");
 end
 
 --[[ Initialize the service ]]--
